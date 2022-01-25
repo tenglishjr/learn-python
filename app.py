a7643deb5b69f1b5e6cd8459cb4dda7cbf1fb341
WUEMQVT <englishtimothy@johndeere.com>
2022-01-25 15:10:23 -0600

    Conditional structures

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
-# Type conversion
+# Conditional Structures
 
-hours = input('Enter hours: ')
-rate = input('Enter rate: ')
-pay = float(hours) * float(rate)
-print('Pay: ', pay)
+x = int(input('Enter a number between 1 and 15: '))
+
+if x < 2:
+    print('small')
+elif x < 10:
+    print('Medium')
+else:
+    print('LARGE')
+
+print('All done')
+
+# Try / Except Structure
+
+try:
+    y = 'Not a number'
+    yy = int(y)
+except:
+    print('That\'s not a number...')
